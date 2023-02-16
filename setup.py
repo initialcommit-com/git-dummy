8448e052f9a53cbffacb89411454de861f505508
Jacob Stopak <jacob@initialcommit.io>
2023-02-15 17:15:52 -0800

    Bump version to 0.0.5
    Signed-off-by: Jacob Stopak <jacob@initialcommit.io>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="git-dummy",
-    version="0.0.4",
+    version="0.0.5",
     author="Jacob Stopak",
     author_email="jacob@initialcommit.io",
     description="Generate dummy Git repositories populated with the desired number of commits, branches, and structure.",
